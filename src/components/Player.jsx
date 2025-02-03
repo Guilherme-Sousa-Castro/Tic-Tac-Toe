8285bb2ee3a3a7d09cdf4810f2586107068ee701
Guilherme <guilherme.sousa.castro.2004@gmail.com>
2025-02-03 11:30:47 +0000

    Criado butao de edit

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,26 @@
+import { useState } from "react";
+
 export default function Player( {name, symbol}){
+    //usaremos state pois queremos alterar as informaçoes dadas e queremos que fiquem guardadas
+    const [isEditing, setIsEditing] = useState(false);
+    
+    function handleEditClick(selectedButton){
+        setIsEditing(true);
+    }
+    let playerName = <span className="player-name">{name}</span>;
+    
+    if(isEditing){
+        playerName =
+           <input type = "text" required></input>
+    }
+
     return (
         <li>
             <span className = "player">
-            <span className="player-name">{name}</span>
+            {playerName}
             <span className="player-symbol">{symbol}</span>
             </span>
-            <button>Edit</button>
+            <button onClick = {handleEditClick}>Edit</button>
           </li>
     );
 }
