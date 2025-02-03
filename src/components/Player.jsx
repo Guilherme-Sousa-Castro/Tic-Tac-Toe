136ffd8646fe15c036d86cd9026f65b4af92b8eb
Guilherme <guilherme.sousa.castro.2004@gmail.com>
2025-02-03 12:14:17 +0000

    Botao de save funcional, ediçao de nomes configurada com exito

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
 
-export default function Player({ name, symbol }) {
+export default function Player({ initialName, symbol }) {
   //usaremos state pois queremos alterar as informaçoes dadas e queremos que fiquem guardadas
   const [isEditing, setIsEditing] = useState(false);
+  const [playerName, setPlayerName] = useState(initialName);
 
-  function handleEditClick(selectedButton) {
+  function handleEditClick() {
     setIsEditing((editing) => !editing);
     /*Alterar entre os diferentes estados, false e true */
     /* ?false : true
@@ -12,22 +13,33 @@ export default function Player({ name, symbol }) {
      * ao alterar um estado tendo em conta o estado anterior deve-se usar uma funçao e nao uma variavel com gatafunhos
      */
   }
-  let playerName = <span className="player-name">{name}</span>;
-  let buttonCaption = "Edit";
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+  //let buttonCaption = "Edit";
 
   if (isEditing) {
-    playerName = <input type="text" required value={name}></input>;
-    buttonCaption = "Save";
+    editablePlayerName = (
+      <input
+        type="text"
+        required
+        value={playerName}
+        onChange={handleChange}
+      ></input>
+    );
+    //buttonCaption = "Save";
   }
 
   return (
     <li>
       <span className="player">
-        {playerName}
+        {editablePlayerName}
         <span className="player-symbol">{symbol}</span>
       </span>
       <button onClick={handleEditClick}>
-        {buttonCaption /*isEditing ? 'Save : 'Edit' */}
+        {/*buttonCaption*/ isEditing ? 'Save' : 'Edit' }
       </button>
     </li>
   );
