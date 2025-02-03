203eef9492da33267dadcb9c54b0a6849a4facc3
Guilherme <guilherme.sousa.castro.2004@gmail.com>
2025-02-03 11:45:01 +0000

    Adicionado população ao carregar para editar e ja se pode salvar (sem a funcao salvar) de volta para editar

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,26 +1,29 @@
 import { useState } from "react";
 
-export default function Player( {name, symbol}){
-    //usaremos state pois queremos alterar as informaçoes dadas e queremos que fiquem guardadas
-    const [isEditing, setIsEditing] = useState(false);
-    
-    function handleEditClick(selectedButton){
-        setIsEditing(true);
-    }
-    let playerName = <span className="player-name">{name}</span>;
-    
-    if(isEditing){
-        playerName =
-           <input type = "text" required></input>
-    }
+export default function Player({ name, symbol }) {
+  //usaremos state pois queremos alterar as informaçoes dadas e queremos que fiquem guardadas
+  const [isEditing, setIsEditing] = useState(false);
 
-    return (
-        <li>
-            <span className = "player">
-            {playerName}
-            <span className="player-symbol">{symbol}</span>
-            </span>
-            <button onClick = {handleEditClick}>Edit</button>
-          </li>
-    );
+  function handleEditClick(selectedButton) {
+    setIsEditing(!isEditing );
+    /*Alterar entre os diferentes estados, false e true */
+    /* ?false : true*/
+  }
+  let playerName = <span className="player-name">{name}</span>;
+  let buttonCaption = 'Edit';
+
+  if (isEditing) {
+    playerName = <input type="text" required value = {name}></input>;
+    buttonCaption = 'Save';
+  }
+
+  return (
+    <li>
+      <span className="player">
+        {playerName}
+        <span className="player-symbol">{symbol}</span> 
+      </span>
+      <button onClick={handleEditClick}>{buttonCaption  /*isEditing ? 'Save : 'Edit' */}</button>
+    </li>
+  );
 }
